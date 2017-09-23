a7b6b54b74d51078f6d8f715675d3c535545e4f9
DonQuixote <askme557@gmail.com>
2017-09-23 14:13:31 +0200

    changed AbstractBaseUser to AbstractUser Cuz the later is actually hashing the password!!

--- instagram/instagram/apps/core/models.py
+++ instagram/instagram/apps/core/models.py
@@ -3,16 +3,16 @@ from passlib.hash import pbkdf2_sha256 '''
 
 from django.db import models
 from location_field.models.plain import PlainLocationField
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 
 #Documentation is a headache for me :3
 # Create your models here.
 '''
-user model that's Inheriting from AbstractBaseUser which is a the django
-Built-in User model, it's very handy because it takes care of storing passwords
+user model that's Inheriting from AbstractUser which is a django Built-in
+User model, it's very handy because it takes care of storing/hashing passwords
 and Authenticating users.
 '''
-class User(AbstractBaseUser):
+class User(AbstractUser):
     first_name = models.CharField(max_length=60) #it's      obvious
     last_name = models.CharField(max_length=60)  #    Pretty       :3
     birthday = models.DateField() #obvious too :3
@@ -27,8 +27,8 @@ class User(AbstractBaseUser):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=64, unique=True)
     '''
-    the below is the required Authentication field by AbstractBaseUser, its value
-    is the EmailField Cuz I choose it to force the user to authentuicate using it.
+    the below is the required Authentication field by AbstractUser, its value is
+    the EmailField Cuz I choose it to force the user to authentuicate using it.
     the password is Handled Automatically by the module.
     '''
     USERNAME_FIELD = 'email'
