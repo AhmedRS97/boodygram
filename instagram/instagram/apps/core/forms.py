c22c45a57cde43a70af33fc0e892b915cd0904bf
DonQuixote <askme557@gmail.com>
2017-09-30 13:29:27 +0200

    added (LoginForm) login form :)

--- instagram/instagram/apps/core/forms.py
+++ instagram/instagram/apps/core/forms.py
@@ -44,3 +44,19 @@ class RegisterForm(forms.ModelForm):
             'placeholder': 'Password',
             }),
         }
+
+# User Login Form
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+        widgets = {
+            'username': forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Username',
+            }),
+            'password': forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Password',
+            }),
+        }
