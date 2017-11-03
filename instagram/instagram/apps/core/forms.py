06e9ee2c11ac669b6922be5cc2edd909c5913ed0
DonQuixote <askme557@gmail.com>
2017-11-03 17:48:01 +0200

    Added the widget variable to PostForm's Meta class

--- instagram/instagram/apps/core/forms.py
+++ instagram/instagram/apps/core/forms.py
@@ -8,6 +8,13 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ['description','tag_user','address','location','image']
         exclude = ['comments']
+        widgets = {'description': forms.Textarea(attrs={
+            'class': 'form-control', 'placeholder':'Type anything.'}),
+        'tag_user': forms.Select(attrs={
+            'class': 'form-control'}),
+        'address': forms.TextInput(attrs={
+            'class': 'form-control', 'placeholder':'Write your address'})
+        }
 
 # User Registration Form
 class RegisterForm(forms.ModelForm):
@@ -16,33 +23,21 @@ class RegisterForm(forms.ModelForm):
         fields = ['email','username','first_name','last_name','birthday','gender','password']
         widgets = {
             'email': forms.EmailInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Email',
-            }),
+                'class': 'form-control', 'placeholder': 'Email'}),
             'first_name': forms.TextInput(attrs={
-            'class': 'form-control fname',
-            'placeholder': 'First Name',
-            }),
+                'class': 'form-control fname', 'placeholder': 'First Name'}),
             'last_name': forms.TextInput(attrs={
-            'class': 'form-control lname',
-            'placeholder': 'Last Name',
-            }),
+                'class': 'form-control lname', 'placeholder': 'Last Name'}),
             'username': forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Username',
-            }),
+                'class': 'form-control', 'placeholder': 'Username'}),
             # the below line is optional attributes for Date widget
             #attrs={'class': 'form-control','placeholder': 'BirthDate',}),
             'birthday': forms.SelectDateWidget(years=[y for y in range(1950, date.today().year+1)]),
 
             'gender': forms.Select(attrs={
-            'class': 'form-control',
-            'placeholder': 'Gender',
-            }),
+                'class': 'form-control', 'placeholder': 'Gender'}),
             'password': forms.PasswordInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Password',
-            }),
+                'class': 'form-control', 'placeholder': 'Password'}),
         }
 
 # User Login Form
