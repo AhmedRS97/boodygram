adee4a3d63300bcac1f90812e2aa8d6455f005af
DonQuixote <askme557@gmail.com>
2017-11-06 01:08:37 +0200

    added the UserHome view (i'll change the view name user's profile soon)

--- instagram/instagram/apps/core/views.py
+++ instagram/instagram/apps/core/views.py
@@ -4,6 +4,7 @@ from .forms import * # importing the forms
 from django.contrib.auth.decorators import login_required #good decorator
 from django.contrib.auth import authenticate, login, logout
 from django.core.paginator import Paginator # the good ol' paginator :D
+from .miscellaneous import * # my custom miscellaneous functions
 
 # Create your views here.
 
@@ -104,6 +105,45 @@ password fields are found.. I think this approach is weak!!.
         if len(request.POST) <= 3 and 'username' in request.POST and 'password' in request.POST:
             return Login(request)
     return redirect('MainPage')
+
+#@login_required #it modifies the url. so it make an error for anonymous user
+def UserHome(request, username):
+    '''
+this view takes a request and username arguments, the username argument is taken
+from the requested url. this view will get the number of the followers, followed
+and posts to use it to render user info, then it query the posts and paginate.
+if the user is authenticated it will check if the user is visiting his own profile,
+else it will check if the user who made the request is following the wanted user.
+finally all this variables will sent as context to the template.
+NOTE: the template have UI logic in it, that it will show things differently.
+    '''
+    user = User.objects.get(username=username)
+    posts_num= Post.objects.filter(user=user).count() #count is QuerySet method
+    followed_num= Follow.objects.filter(follower=user).count()
+    followers_num= Follow.objects.filter(followed=user).count()
+    # creating a paginator object that split the posts to dozens.
+    paginator= Paginator(Post.objects.filter(user=user).order_by('-created'), 12)
+    try:
+        page = int(request.GET.get('page', '1'))
+    except:
+        page = 1
+    try:
+        posts = paginator.page(page)
+    except(EmptyPage, InvalidPage):
+        posts = paginator.page(paginator.num_pages)
+    if request.user.is_authenticated:
+        same_user= True if request.user.username == username else False
+        is_followed= get_or_none(Follow,follower=request.user,followed=user) if same_user else None
+        return render(request, 'user/user-profile.html',{
+            'request':request, 'posts':posts, 'same_user':same_user,
+            'username':username,'followers':followers_num,
+            'following':followed_num,'is_followed':is_followed,
+            'posts_num':posts_num,'user':user })
+    else:
+        return render(request, 'user/user-profile.html',{
+            'request':request, 'posts':posts, 'user':user,
+            'followers':followers_num, 'following':followed_num,
+            'username':username,'posts_num':posts_num,'same_user':None})
 
 #@login_required #the login_required decorator > will check if user is logged in
 def PostFormView(request, username):
