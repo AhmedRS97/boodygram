428e863e6de97c371887fd9721d8431e42d23027
DonQuixote <askme557@gmail.com>
2017-11-05 02:27:46 +0200

    edited the imports and added a Register view.

--- instagram/instagram/apps/core/views.py
+++ instagram/instagram/apps/core/views.py
@@ -1,9 +1,37 @@
-from django.shortcuts import render, redirect # it's Crystal clear :3
-from .models import Post, User # importing the post model
-from .forms import PostForm # importing the PostForm
+from django.shortcuts import render, redirect
+from .models import * # importing the models
+from .forms import * # importing the forms
 from django.contrib.auth.decorators import login_required #good decorator
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+
+#Register view
+def Register(request):
+    '''
+    a Register view that validate a form's data and save it to database, then
+    it authenticate and login the user and redirect to the user profile page.
+    if the form's data is invalid it will render the same page and form's data
+    but with warnings.
+    '''
+    form = RegisterForm(request.POST)
+    if form.is_valid():
+        #(commit=False) means that it will keep the data but will not commit it.
+        # thus enabling us to modify its values before the final commit.
+        user = form.save(commit=False)
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user.set_password(password)
+        user.save()
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect('/'+username)
+    else:
+        return render(request, 'base.html', {
+            'signupform':form,
+            'loginform':LoginForm()})
 
 #main page view
 def MainPage(request):
